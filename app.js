cc46b0309d8aa84c06232605e8d0bbc757bcf818
Lucas Passeri <lucaspasseri@poli.ufrj.br>
2025-05-26 13:41:16 -0300

    Add validation constraint
    Add the password must be equal to confirmPassword constraint

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const form = document.querySelector("#new-account-form");
 
-console.log({ form });
-
 for (let i = 0; i < form.length - 1; i++) {
 	const currInput = form[i];
 	currInput.addEventListener("input", validateInput);
@@ -9,10 +7,22 @@ for (let i = 0; i < form.length - 1; i++) {
 
 function validateInput(event) {
 	const target = event.target;
-	console.log({ target });
 
 	const valid = target.validity.valid;
 	const span = target.parentNode.querySelector("span span");
+
+	if (target.id === "confirm-password" && valid) {
+		const password = document.querySelector("#password");
+
+		if (password.value !== target.value) {
+			target.className = "invalid";
+			span.textContent = "O passaword inserido deve ser igual ao anterior.";
+		} else {
+			target.className = "valid";
+			span.textContent = target.validationMessage;
+		}
+		return;
+	}
 
 	if (valid) {
 		target.className = "valid";
@@ -32,10 +42,24 @@ form.addEventListener("submit", event => {
 
 	const invalidInputs = inputs.filter(input => !input.validity.valid);
 
-	if (invalidInputs.length > 0) {
-		event.preventDefault();
-		addInvalidateStyles(invalidInputs);
+	if (invalidInputs.length === 0) {
+		const confirmPassword = inputs[inputs.length - 1];
+		const password = inputs[inputs.length - 2];
+		const span = confirmPassword.parentNode.querySelector("span span");
+
+		if (password.value !== confirmPassword.value) {
+			event.preventDefault();
+			confirmPassword.className = "invalid";
+			span.textContent = "O passaword inserido deve ser igual ao anterior.";
+		} else {
+			confirmPassword.className = "valid";
+			span.textContent = confirmPassword.validationMessage;
+		}
+		return;
 	}
+
+	event.preventDefault();
+	addInvalidateStyles(invalidInputs);
 });
 
 function addInvalidateStyles(inputs) {
@@ -46,5 +70,7 @@ function addInvalidateStyles(inputs) {
 		}
 
 		input.className = "invalid";
+		const span = input.parentNode.querySelector("span span");
+		span.textContent = input.validationMessage;
 	}
 }
